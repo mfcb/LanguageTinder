c203ab5ab88a5a7f4d374bc131dca12a8ea9a6db
mfcb <mb@markusbuhl.com>
2017-11-02 15:47:45 +0100

    Initial Commit

---
+++ LanguageTinder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LanguageTinder
+//
+//  Created by Markus Buhl on 02.11.17.
+//  Copyright © 2017 Markus Buhl. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
